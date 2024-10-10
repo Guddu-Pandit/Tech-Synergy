d352ce2c14e4402b3429a34cd3001a863f9af168
Guddu Pandit <guddukrp769@gmail.com>
2024-10-11 01:11:20 +0530

    no right click

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+        // ----For preload----
 const loader = document.querySelector(".loader")
 
 window.addEventListener("load", preload);
@@ -7,8 +8,7 @@ function preload() {
 }
 
 
-
-
+        // ----For nav ----
 const navlinks = document.querySelector(".nav-links");
 navlinks.style.maxHeight = "0px";
 const menuicon = document.querySelector("i");
@@ -21,3 +21,9 @@ menuicon.addEventListener("click", () => {
         navlinks.style.maxHeight = "0px";
     }
 });
+
+
+    // ----For No one right click for code----
+// document.addEventListener("contextmenu", function(e){
+//     e.preventDefault()
+// },false);
