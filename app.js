cfe2543c3c9dec9a0842a08b90a0eaaa379c9f5c
Guddu Pandit <guddukrp769@gmail.com>
2024-10-23 23:33:51 +0530

    fixing faq

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
-        // ----For preload----
+// ----For preload----
 const loader = document.querySelector(".loader");
-
 window.addEventListener("load", preload);
-
 function preload() {
-   loader.classList.add("disppear");
+  loader.classList.add("disppear");
 }
 
-
-        // ----For nav ----
+// ----For nav ----
 const navlinks = document.querySelector(".nav-links");
 navlinks.style.maxHeight = "0px";
 const menuicon = document.querySelector("i");
 menuicon.addEventListener("click", () => {
-    if(navlinks.style.maxHeight == "0px")
-    {
-        navlinks.style.maxHeight = "350px";
-    }
-    else{
-        navlinks.style.maxHeight = "0px";
-    }
+  if (navlinks.style.maxHeight == "0px") {
+    navlinks.style.maxHeight = "350px";
+  } else {
+    navlinks.style.maxHeight = "0px";
+  }
 });
 
+                                      //  ----For FAQ ----
 
-             //  ----For FAQ ----
 const accordionitem = document.querySelectorAll(".accordion");
-const symbol = document.querySelector("span");
+const symbol = document.querySelectorAll("span");
 
 for (i=0 ; i<accordionitem.length; i++){
     accordionitem[i].addEventListener('click', function(){
       let result = this.classList.toggle('active')
         if(result){
-            remove.classList('active')
+            remove.classList('active');
+        }
+        else{
         }
     })
 }
 
 
-    // ----For No one right click for code----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----For No one right click for code----
 // document.addEventListener("contextmenu", function(e){
 //     e.preventDefault()
 // },false);
